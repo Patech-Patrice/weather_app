24022278cb41663d005f5bd952c2538d1c4e5aaa
Patrice Drayton <patricedrayton@Patrices-MacBook-Pro.local>
2019-07-16 14:50:11 -0400

    added images and assets

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, Platform, TextInput } from 'react-native';
+import { StyleSheet, View, ImageBackground, Text, KeyboardAvoidingView, Platform} from 'react-native';
+
+import getImageForWeather from './utils/getImageForWeather.js'
+import SearchInput from './components/SearchInput';
+
+
+// in this component we're importing a getImageForWeather method from our utils directory which returns a specifric image from the assets directory depending on the weather type.
+// the ImageBackground component is a view with an image nested within.  The source prop accepts an image location; uses the prop style for styling the view container and the prop imagestyle for styling the image itself.
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
-        <Text style={[styles.smallText, styles.textStyle]}> Light Cloud</Text>
-        <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+    <KeyboardAvoidingView style={styles.container}
+      behavior="padding"
+      >
+        <ImageBackground 
+         source={getImageForWeather('Clear')}
+         style={styles.imageContainer}
+         imageStyle={styles.image} >
 
-        <TextInput
-          autocorrect={false}
-          placeholder="Search any city"
-          placeholderTextColor="white"
-          style={styles.textInput}
-          clearButtonMode="always"
-          />
-      </View>
+        <View style={styles.detailsContainer}>
+          
+            
+             <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
+             <Text style={[styles.smallText, styles.textStyle]}> Light Cloud</Text>
+             <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+
+             <SearchInput placeholder="Search any City" />
+        </View>     
+          </ImageBackground>  
+        
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -24,14 +38,29 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#34495E',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
+  },
   textStyle: {
     textAlign: 'center',
-    fontFamily: 
-    Platform.OS === "ios" ? 'AvenirNext-Regular' : 'Roboto',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    color: 'white',
   },
   largeText: {
     fontSize: 44,
@@ -39,15 +68,4 @@ const styles = StyleSheet.create({
   smallText: {
     fontSize: 18,
   },
-  textInput: {
-    backgroundColor: '#666',
-    color: 'white',
-    height: 40,
-    width: 300,
-    marginTop: 20,
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    alignSelf: 'center',
-  },
-  
 });
